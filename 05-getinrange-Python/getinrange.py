81f11da476b44c49c854fc14412d8c6c45866fcb
Naveen Sai Ram <naveenkatragadda623@gmail.com>
2020-07-18 21:39:07 +0530

    Jul 18  21:39:06

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -6,4 +6,11 @@
 
 def fun_getinrange(x, bound1, bound2):
 	# your code goes here
-	return -1
+	if(x > bound1 and x < bound2):
+		return x
+
+	elif x < bound1:
+		return bound1
+		
+	else:
+		return bound2
