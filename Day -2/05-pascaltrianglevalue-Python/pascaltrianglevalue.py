e639c26edd7a58b13c21d4d3b9bcbcee8c9a59f8
Naveen Sai Ram <naveenkatragadda623@gmail.com>
2020-07-19 11:03:44 +0530

    Jul 19  11:03:43

---
+++ Day -2/05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -0,0 +1,17 @@
+# Write the function pascalsTriangleValue(row, col) 
+# that takes int values row and col, and returns the 
+# value in the given row and column of Pascal's 
+# Triangle where the triangle starts at row 0, and 
+# each row starts at column 0. If either row or col 
+# are not legal values, return None, instead of crashing. 
+
+
+
+import math
+def fun_pascaltrianglevalue(row, col):
+      if(col > row):
+            return 0
+      else:
+            return math.factorial(row) / math.factorial(col) * math.factorial(row - col)
+    			
+	# return 1
