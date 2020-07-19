535132a7eba2364dc2492471c53eab4e0ffd1c8a
Naveen Sai Ram <naveenkatragadda623@gmail.com>
2020-07-19 11:27:54 +0530

    Jul 19  11:27:54

---
+++ Day -2/08-nearestodd-Python/nearestodd.py
@@ -0,0 +1,17 @@
+# Write the function nearestOdd(n) that takes an float n, 
+# and returns as an int value the nearest odd number to n. 
+# In the case of a tie, return the smaller odd value. 
+# Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
+
+
+
+def fun_nearestodd(n):
+      if n % 2 == 0:
+            return round(n - 1)
+      elif n % 2 == 1:
+            return round(n)
+      else:
+            return round((n - (n % 2) + 1))
+	# return 0
+
+
