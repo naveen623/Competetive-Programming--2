1e2c3c2148620bb260027662266d1752d9ed7d5d
Naveen Sai Ram <naveenkatragadda623@gmail.com>
2020-07-21 08:31:58 +0530

    largestperfectsquare done

---
+++ Day -2/02-queues-Python/queues.py
@@ -0,0 +1,21 @@
+"""Make a Queue class using a list!
+Hint: You can use any Python list method
+you'd like! Try to write each one in as 
+few lines as possible.
+Make sure you pass the test cases too!"""
+
+class Queue:
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+        # pass
+
+    def peek(self):
+        return self.storage[0]
+        # pass 
+
+    def dequeue(self):
+        return self.storage.pop(0)
+        # pass
