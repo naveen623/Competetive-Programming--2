57081dab679c2bc37737221e1dd9d48701e4d6cc
Naveen Sai Ram <naveenkatragadda623@gmail.com>
2020-07-27 10:44:06 +0530

    Jul 27  10:44:06

---
+++ Day -10/06-sumofsquaresofdigits-Python/sumofsquaresofdigit.py
@@ -0,0 +1,15 @@
+# Write the function sumOfSquaresOfDigits(n) which takes a non-negative integer and returns the 
+# sum of the squares of its digits. Here are some test assertions for you:
+# assert(sumOfSquaresOfDigits(5) == 25)   # 5**2 = 25
+# assert(sumOfSquaresOfDigits(12) == 5)   # 1**2 + 2**2 = 1+4 = 5
+# assert(sumOfSquaresOfDigits(234) == 29) # 2**2 + 3**2 + 4**2 = 4 + 9 + 16 = 29
+
+def sumofsquaresofdigit(n):
+      sum = 0
+      while n > 0:
+            rem = n % 10
+            sum += rem * rem
+            n = n // 10
+      return sum
+	# Your code goes here
+	# pass
